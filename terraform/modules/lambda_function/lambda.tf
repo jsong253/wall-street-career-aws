66c02cddd38ab4eda7acace4a52ad10a83f0352d
jsong253 <songjianzhong16@gmail.com>
2023-05-25 16:49:18 -0400

    update lambda.tf not to use s3 to archive lamnda zip file

--- terraform/modules/lambda_function/lambda.tf
+++ terraform/modules/lambda_function/lambda.tf
@@ -1,46 +1,59 @@
 //using archive_file data source to zip the lambda code:
 // The below code would generate a function_code.zip file inside the lambda_function module.
-data "archive_file" "lambda_code" {
-  type        = "zip"
-  source_dir  = "${path.module}/"
-  output_path = "${path.module}/function_code.zip"
-}
+# data "archive_file" "lambda_code" {
+#   type        = "zip"
+#   source_dir  = "${path.module}/"
+#   output_path = "${path.module}/function_code.zip"
+# }
 
-resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = var.s3_bucket_name
-}
-//making the s3 bucket private as it houses the lambda code:
-resource "aws_s3_bucket_acl" "lambda_bucket_acl" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-  acl    = "private"
-  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]       // to resolve AccessControlListNotSupported: The bucket does not allow ACLs status code: 400
-}
+# resource "aws_s3_bucket" "lambda_bucket" {
+#   bucket = var.s3_bucket_name
+# }
+# //making the s3 bucket private as it houses the lambda code:
+# resource "aws_s3_bucket_acl" "lambda_bucket_acl" {
+#   bucket = aws_s3_bucket.lambda_bucket.id
+#   acl    = "private"
+#   depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]       // to resolve AccessControlListNotSupported: The bucket does not allow ACLs status code: 400
+# }
 
-# Resource to avoid error "AccessControlListNotSupported: The bucket does not allow ACLs"
-# https://stackoverflow.com/questions/76049290/error-accesscontrollistnotsupported-when-trying-to-create-a-bucket-acl-in-aws
-resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-  rule {
-    object_ownership = "ObjectWriter"
-  }
-}
+# # Resource to avoid error "AccessControlListNotSupported: The bucket does not allow ACLs"
+# # https://stackoverflow.com/questions/76049290/error-accesscontrollistnotsupported-when-trying-to-create-a-bucket-acl-in-aws
+# resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+#   bucket = aws_s3_bucket.lambda_bucket.id
+#   rule {
+#     object_ownership = "ObjectWriter"
+#   }
+# }
 
-resource "aws_s3_object" "lambda_code" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-  key    = "function_code.zip"
-  source = data.archive_file.lambda_code.output_path
-  etag   = filemd5(data.archive_file.lambda_code.output_path)
-}
+# resource "aws_s3_object" "lambda_code" {
+#   bucket = aws_s3_bucket.lambda_bucket.id
+#   key    = "function_code.zip"
+#   source = data.archive_file.lambda_code.output_path
+#   etag   = filemd5(data.archive_file.lambda_code.output_path)
+# }
 
+// archive lambda code with s3
+# resource "aws_lambda_function" "lambda_function" {
+#   filename = data.archive_file.get_registrations_lambda_archive_file.output_path
+#   function_name    = var.lambda_function_name
+#   description      = var.lambda_function_name
+#   s3_bucket        = aws_s3_bucket.lambda_bucket.id                         // used with s3 bucket
+#   s3_key           = aws_s3_object.lambda_code.key                          // used with s3 bucket
+#   source_code_hash = data.archive_file.lambda_code.output_base64sha256      // used with s3 bucket
+#   runtime          = "nodejs14.x"
+#   handler          = "modules/lambda_function/index.handler"
+#   role             = aws_iam_role.lambda_execution_role.arn
+# }
+
+
+// archive lambda code without s3
 resource "aws_lambda_function" "lambda_function" {
-  // filename = data.archive_file.get_registrations_lambda_archive_file.output_path
+  filename = data.archive_file.get_registrations_lambda_archive_file.output_path
   function_name    = var.lambda_function_name
-  s3_bucket        = aws_s3_bucket.lambda_bucket.id
-  s3_key           = aws_s3_object.lambda_code.key
+  description      = var.lambda_function_name
   runtime          = "nodejs14.x"
-  handler          = "index.handler"
-  source_code_hash = data.archive_file.lambda_code.output_base64sha256
-  // source_code_hash = data.archive_file.get_registrations_lambda_archive_file.output_base64sha256
+  handler          = "modules/lambda_function/index.handler"
+  source_code_hash = data.archive_file.get_registrations_lambda_archive_file.output_base64sha256        
   role             = aws_iam_role.lambda_execution_role.arn
 
   environment{
@@ -78,13 +91,13 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# alternate way to archive lambda without using s3 bucket to store lambda zip file
-# data "archive_file" "get_registrations_lambda_archive_file" {
-#   type        = "zip"
- 
-#   output_path = "${path.module}/get_registrations_lambda_archive_file.zip"
-#   source {
-#     content     = "${file("modules/lambda_function/index.js")}"
-#     filename    = "modules/lambda_function/index.js"
-#   }
-# }
+// alternate way to archive lambda without using s3 bucket to store lambda zip file
+// used without s3
+data "archive_file" "get_registrations_lambda_archive_file" {
+  type        = "zip"
+  output_path = "${path.module}/get_registrations_lambda_archive_file.zip"
+  source {
+    content   = "${file("terraform/modules/lambda_function/index.js")}"         // must be the full path
+    filename  = "modules/lambda_function/index.js"
+  }
+}
